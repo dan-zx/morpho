419739ee555e22f010d6e044e99b6c44318f93c9
dan-zx <danyboy_shaggy@hotmail.com>
2014-05-02 11:40:05 -0500

    Project setup

---
+++ src/com/morpho/android/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.morpho.android.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.morpho.android.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
